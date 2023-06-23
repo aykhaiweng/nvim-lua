ef617f241d42572ce7fda5f05cdedce0fd85a639
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-06-23 14:24:34 +0800

    feat: Adding folding options for treesitter

--- lua/config/treesitter/init.lua
+++ lua/config/treesitter/init.lua
@@ -40,6 +40,37 @@ require('nvim-treesitter.configs').setup({
 -- Folding fun times
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+------------------------------------------------------------------------------------ o
+-- Copy and pasted from https://www.jmaguire.tech/posts/treesitter_folding/
+local vim = vim
+local api = vim.api
+local M = {}
+-- function to create a list of commands and convert them to autocommands
+-------- This function is taken from https://github.com/norcalli/nvim_utils
+function M.nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        api.nvim_command('augroup '..group_name)
+        api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            api.nvim_command(command)
+        end
+        api.nvim_command('augroup END')
+    end
+end
+
+
+local autoCommands = {
+    -- other autocommands
+    open_folds = {
+        {"BufReadPost,FileReadPost", "*", "normal zR"}
+    }
+}
+
+M.nvim_create_augroups(autoCommands)
+------------------------------------------------------------------------------------ o
+
 vim.opt.foldlevel = 99
 
 local OpenFolds = vim.api.nvim_create_augroup(
