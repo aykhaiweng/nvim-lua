d75a75477f7a3e5967cd7071807a8bb6473eab71
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-11-16 16:09:00 +0800

    chore: Cleanup

--- init.lua
+++ init.lua
@@ -11,15 +11,3 @@ require("lazy_init")
 require("core/options")
 require("core/keymaps")
 require("core/colors")
--- Plugin Configurations
--- require("config/fidget") -- LSP status
--- require("config/luasnip") -- Snippets
-
-
--- vim.cmd([[
---     augroup ReloadConfig
---         autocmd!
---         autocmd BufWritePost *.lua source $MYVIMRC
---         autocmd BufWritePost *.lua lua require("lazy.core.loader").reload(require("lazy.core.config").plugins["heirline.nvim"])
---     augroup END
--- ]])
