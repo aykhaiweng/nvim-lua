24e0a80b94398b804ea8bb331804ca1dc597aac2
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2023-11-23 10:04:30 +0800

    chore: Added configuration for fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,6 +34,14 @@ return {
                         height = 0.8
                     }
                 },
+                extensions = {
+                    fzf = {
+                      fuzzy = true,                    -- false will only do exact matching
+                      override_generic_sorter = true,  -- override the generic sorter
+                      override_file_sorter = true,     -- override the file sorter
+                      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                    }
+                }
             })
 
             -- remaps
