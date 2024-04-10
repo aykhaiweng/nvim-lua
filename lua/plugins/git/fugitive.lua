186bf93c510c4cd90b3fb9a847f46f59348bda6f
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-04-10 12:05:18 +0800

    feature: Added keymaps for fugitive
    Also added keymaps for Telescope related functions

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -1,8 +1,17 @@
 return {
-    {
-        "tpope/vim-fugitive",
+	{
+		"tpope/vim-fugitive",
 		keys = function()
-			return {}
+			return {
+				{ "<leader>gg", [[:<C-U>Git<CR>]], "n", desc = "Fugitive" },
+				-- { "<leader>gm", [[:<C-U>Git mergetool<CR>]], "n", { desc = "Fugitive" } },
+				-- { "<leader>gd", [[:<C-U>Git difftool<CR>]], "n", { desc = "Fugitive" } },
+				{ "<leader>gb", [[:<C-U>Git blame<CR>]], "n", desc = "Git blame" },
+				{ "<leader>gl", [[:<C-U>Git log<CR>]], "n", desc = "Git log" },
+				{ "<leader>pc", [[:<C-U>Telescope git_commits<CR>]], "n", desc = "Git commits" },
+				{ "<leader>ps", [[:<C-U>Telescope git_stash<CR>]], "n", desc = "Git stash" },
+				{ "<leader>pb", [[:<C-U>Telescope git_branches<CR>]], "n", desc = "Git branches" },
+			}
 		end,
-    }
+	},
 }
