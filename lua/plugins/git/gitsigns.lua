db6961a8d477913810c03eefed3c86214b8c48d2
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-04-10 12:04:19 +0800

    fix: Removed problematic keymaps

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -75,21 +75,21 @@ return {
 					end
 
 					-- Navigation
-					map("n", "]c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]c", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
-					end)
-
-					map("n", "[c", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[c", bang = true })
-						else
-							gitsigns.nav_hunk("prev")
-						end
-					end)
+					-- map("n", "]c", function()
+					-- 	if vim.wo.diff then
+					-- 		vim.cmd.normal({ "]c", bang = true })
+					-- 	else
+					-- 		gitsigns.nav_hunk("next")
+					-- 	end
+					-- end)
+					--
+					-- map("n", "[c", function()
+					-- 	if vim.wo.diff then
+					-- 		vim.cmd.normal({ "[c", bang = true })
+					-- 	else
+					-- 		gitsigns.nav_hunk("prev")
+					-- 	end
+					-- end)
 
 					-- Actions
 					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
