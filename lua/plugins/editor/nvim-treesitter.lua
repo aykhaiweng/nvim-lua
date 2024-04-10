a7374256ef563ab0d9956ba87e253b2eb50da376
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-04-10 13:35:23 +0800

    feature: Added sphinx to Treesitter

--- lua/plugins/editor/nvim-treesitter.lua
+++ lua/plugins/editor/nvim-treesitter.lua
@@ -8,6 +8,7 @@ return {
 			require("nvim-treesitter.configs").setup({
 				-- A list of parser names, or "all" (the four listed parsers should always be installed)
 				ensure_installed = {
+					"dockerfile",
 					"python",
 					"javascript",
 					"lua",
@@ -18,6 +19,7 @@ return {
 					"json",
 					"markdown",
 					"markdown_inline",
+					"rst",
 				},
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
@@ -31,13 +33,7 @@ return {
 				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
 				highlight = {
-					-- `false` will disable the whole extension
 					enable = true,
-
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
 					additional_vim_regex_highlighting = false,
 				},
 				indent = {
@@ -54,4 +50,6 @@ return {
 	},
 	-- Playground
 	{ "nvim-treesitter/playground" },
+	-- Sphinx
+	{ "stsewd/sphinx.nvim" },
 }
