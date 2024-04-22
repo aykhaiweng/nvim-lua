e647bdeb53df9a56d47d3775a772c4467b859093
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-04-22 08:23:13 +0800

    conf: Disabled lsp_signature

--- lua/plugins/dev/lsp_signature.lua
+++ lua/plugins/dev/lsp_signature.lua
@@ -4,6 +4,7 @@ return {
 		"ray-x/lsp_signature.nvim",
 		cmd = { "LspInfo", "LspInstall", "LspStart" },
 		event = { "BufReadPre", "BufNewFile" },
+        enabled = false,
 		opts = {
 			floating_window_above_cur_line = false,
 			floating_window_off_y = -2,
