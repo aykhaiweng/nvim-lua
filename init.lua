15446341b2a828590400213cdcecec4e6c5e3218
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-05-12 07:45:42 +0800

    refactor: Moved neovide to gui

--- init.lua
+++ init.lua
@@ -10,9 +10,5 @@ vim.g.mapleader = " "
 require("core/options")
 require("core/keymaps")
 require("core/colors")
+require("core/gui")
 require("lazy_init")
-
--- Neovide shit
-if vim.g.neovide then
-    require("core/neovide")
-end
