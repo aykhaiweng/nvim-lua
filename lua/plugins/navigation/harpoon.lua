5748270c82e4d61311fe353808b456efc9c3a61e
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-10-09 11:55:41 +0800

    fature: Added telescope for harpoon

--- lua/plugins/navigation/harpoon.lua
+++ lua/plugins/navigation/harpoon.lua
@@ -6,9 +6,27 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = function()
 			local harpoon = require("harpoon")
+
+            local conf = require("telescope.config").values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require("telescope.pickers").new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                }):find()
+            end
+
 			return {
 				{
-					"<leader>a",
+					"<C-a>",
 					function()
 						harpoon:list():add()
 					end,
@@ -17,18 +35,18 @@ return {
 				},
 				{
 					"<C-e>",
-					function()
-						harpoon.ui:toggle_quick_menu(harpoon:list())
-					end,
+                    function ()
+                        toggle_telescope(harpoon:list())
+                        -- harpoon.ui:toggle_quick_menu(harpoon:list())
+                    end,
 					"n",
 					desc = "Open Harpoon",
 				},
 			}
 		end,
-		config = function()
+		config = function(_, opts)
 			local harpoon = require("harpoon")
-
-			harpoon:setup({})
+			harpoon:setup(opts)
 		end,
 	},
 }
