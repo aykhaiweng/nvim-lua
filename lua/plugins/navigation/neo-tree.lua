ec135bf717181664fd18485cecac03c9c6a0ffa7
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2024-11-08 11:41:00 +0800

    conf: Neo-tree now ignores Outline

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -11,13 +11,14 @@ return {
 			version = "2.*",
 			config = function()
 				require("window-picker").setup({
+                    hint = 'floating-big-letter',
 					filter_rules = {
 						include_current_win = false,
 						autoselect_one = true,
 						-- filter using buffer options
 						bo = {
 							-- if the file type is one of following, the window will be ignored
-							filetype = { "neo-tree", "neo-tree-popup", "notify", "edgy", "terminal" },
+							filetype = { "neo-tree", "neo-tree-popup", "notify", "edgy", "terminal", "Outline" },
 							-- if the buffer type is one of following, the window will be ignored
 							buftype = { "terminal", "quickfix" },
 						},
