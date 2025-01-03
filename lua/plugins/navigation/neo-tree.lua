bbf4a1d1e71c2f493b4aceee5cb47e55496eeb5c
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2025-01-03 13:45:29 +0800

    fix: Something neotree asked me to do?

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -28,6 +28,9 @@ return {
 		},
 	},
 	opts = {
+        rocks = {
+            hererocks = false
+        },
 		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
 		popup_border_style = "rounded",
 		enable_git_status = true,
