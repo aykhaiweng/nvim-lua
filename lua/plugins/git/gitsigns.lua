326b195da3f2207838d637524f9298b5ca077045
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2025-02-13 18:13:05 +0800

    conf: Changed keybindings for gitsigns

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -80,31 +80,31 @@ return {
 					end)
 
 					-- Actions
-					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
-					map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
-					map("v", "<leader>hs", function()
+					map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+					map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+					map("v", "<leader>gs", function()
 						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 					end, { desc = "Stage selection" })
-					map("v", "<leader>hr", function()
+					map("v", "<leader>gr", function()
 						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 					end, { desc = "Reset selection" })
-					map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
-					map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
-					map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
-					map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
-					map("n", "<leader>hb", function()
+					map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+					map("n", "<leader>gu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+					map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+					map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+					map("n", "<leader>gb", function()
 						gitsigns.blame_line({ full = true })
 					end, { desc = "Blame line" })
-					map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
-					map("n", "<leader>hD", function()
+					map("n", "<leader>gd", gitsigns.diffthis, { desc = "Diff this" })
+					map("n", "<leader>gD", function()
 						gitsigns.diffthis("~")
 					end, { desc = "Diff this ~" })
 
 					-- Toggle Virtual Text
-					map("n", "<leader>vb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
-					map("n", "<leader>vd", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+					map("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+					map("n", "<leader>gd", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
 
-					-- Text object
+					-- Text objecht
 					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 				end,
 			})
