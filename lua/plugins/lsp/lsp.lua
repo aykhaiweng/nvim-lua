e269b37ca96d25fbd4a1b34c86baa98e2c25bf84
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-21 10:56:56 +0000

    conf: Disabled virtual lines

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -56,9 +56,7 @@ return {
 			--- diagnostics
 			vim.diagnostic.config({
 				virtual_text = true,
-				virtual_lines = {
-					current_line = true,
-				},
+				virtual_lines = false,
 				underline = true,
 				update_in_insert = false,
 				float = {
@@ -86,6 +84,34 @@ return {
 					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", _opts)
 					vim.keymap.set("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", _opts)
 				end,
+			})
+
+            --- Automatically open diagnostics on cursor hold
+			function OpenDiagnosticIfNoFloat()
+				for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+					if vim.api.nvim_win_get_config(winid).zindex then
+						return
+					end
+				end
+				-- THIS IS FOR BUILTIN LSP
+				vim.diagnostic.open_float({
+					scope = "cursor",
+					focusable = false,
+					close_events = {
+						"CursorMoved",
+						"CursorMovedI",
+						"BufHidden",
+						"InsertCharPre",
+						"WinLeave",
+					},
+				})
+			end
+			-- Show diagnostics under the cursor when holding position
+			vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+			vim.api.nvim_create_autocmd({ "CursorHold" }, {
+				pattern = "*",
+				command = "lua OpenDiagnosticIfNoFloat()",
+				group = "lsp_diagnostics_hold",
 			})
 
 			--- Automatically restart LSP when a file has been saved
