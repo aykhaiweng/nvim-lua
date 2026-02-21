3d165fa849216d15b76e6e4586081d57bd25dee6
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2026-02-21 10:24:14 +0000

    fmt,doc: cleaned up formatting and removed comments

--- lua/plugins/lsp/outline.lua
+++ lua/plugins/lsp/outline.lua
@@ -5,31 +5,20 @@ return {
 		cmd = { "Outline", "OutlineOpen", "OutlineFocus" },
 		opts = {
 			outline_items = {
-				-- Show extra details with the symbols (lsp dependent) as virtual next
 				show_symbol_details = true,
-				-- Show corresponding line numbers of each symbol on the left column as
-				-- virtual text, for quick navigation when not focused on outline.
-				-- Why? See this comment:
-				-- https://github.com/simrat39/symbols-outline.nvim/issues/212#issuecomment-1793503563
 				show_symbol_lineno = false,
-				-- Whether to highlight the currently hovered symbol and all direct parents
 				highlight_hovered_item = true,
-				-- Whether to automatically set cursor location in outline to match
-				-- location in code when focus is in code. If disabled you can use
-				-- `:OutlineFollow[!]` from any window or `<C-g>` from outline window to
-				-- trigger this manually.
 				auto_set_cursor = true,
-				-- Autocmd events to automatically trigger these operations.
 				auto_update_events = {
-					-- Includes both setting of cursor and highlighting of hovered item.
-					-- The above two options are respected.
-					-- This can be triggered manually through `follow_cursor` lua API,
-					-- :OutlineFollow command, or <C-g>.
 					follow = { "CursorHold" },
-					-- Re-request symbols from the provider.
-					-- This can be triggered manually through `refresh_outline` lua API, or
-					-- :OutlineRefresh command.
-					items = { "InsertLeave", "WinEnter", "BufEnter", "BufWinEnter", "TabEnter", "BufWritePost" },
+					items = {
+                        "InsertLeave",
+                        "WinEnter",
+                        "BufEnter",
+                        "BufWinEnter",
+                        "TabEnter",
+                        "BufWritePost",
+                    },
 				},
 			},
 			outline_window = {
@@ -48,6 +37,7 @@ return {
 				down_and_jump = "<M-j>", -- Unbind CTRL-J
 				up_and_jump = "<M-k>", -- Unbind CTRL-K
 			},
+            focus_on_open = false
 		},
 		keys = {
 			{ "<leader>=", "<cmd>OutlineOpen<CR>", "n", desc = "Open Outline" },
